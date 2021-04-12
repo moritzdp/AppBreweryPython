c6bd36e12f20dfb675827da8a01f9f69fd0a84be
moritzdp <69892518+moritzdp@users.noreply.github.com>
2021-04-12 14:23:49 +0800

    Create band-name-generator-app.py

---
+++ band-name-generator-app.py
@@ -0,0 +1,11 @@
+# print("Welcome to the Band Name Generator.")
+# street = input("What's name of the city you grew up in?\n")
+# pet = input("What's your pet's name?\n")
+# print("Your band name could be " + street + " " + pet)
+
+print("Welcome to Zam Naming Wizard")
+street = input("Whats the name of the city you grew up in ?\n ")
+
+pet = input("What is your pet's name?\n")
+
+print("Your band name could be " + street + " " + pet)
