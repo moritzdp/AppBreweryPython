26dd2b7c16007a88dfa1573b3c4780ed152510c2
moritzdp <69892518+moritzdp@users.noreply.github.com>
2021-04-12 17:41:06 +0800

    Update day1-1-testyourcode.py

--- day1-1-testyourcode.py
+++ day1-1-testyourcode.py
@@ -11,6 +11,14 @@ print("The function is declared like this:")
 
 print("print('what to print')")
 
+#possible errors are indentation and spaces, use concat / + sign where psosible
+#do not miss double quotes
+
+#code fix for complex strings
+
+print("Day 1 - String Manipulation\n"+"String Concatenation is done with the "+" sign.\n"+'e.g. print("Hello " + "world")\n'+"New lines can be created with a backslash and n.")
+
+
 
 
 
