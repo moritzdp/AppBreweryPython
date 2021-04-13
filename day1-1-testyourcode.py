e1956d1a97873a05c45ce13f62e582b5cc3cd2f3
moritzdp <69892518+moritzdp@users.noreply.github.com>
2021-04-13 15:22:29 +0800

    Update day1-1-testyourcode.py

--- day1-1-testyourcode.py
+++ day1-1-testyourcode.py
@@ -11,12 +11,32 @@ print("The function is declared like this:")
 
 print("print('what to print')")
 
-#possible errors are indentation and spaces, use concat / + sign where psosible
-#do not miss double quotes
+# Notes
+# possible errors are indentation and spaces, use concat / + sign where psosible
+# do not miss double quotes
 
 #code fix for complex strings
 
 print("Day 1 - String Manipulation\n"+"String Concatenation is done with the "+" sign.\n"+'e.g. print("Hello " + "world")\n'+"New lines can be created with a backslash and n.")
+
+# Try on Thonny.org
+# input("A prompt for the user")
+
+#Write your code below this line 👇
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string. 
+
+print(len(input("Enter your username for calcualation:\n")))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
